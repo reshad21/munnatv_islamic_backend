2bf21c5b109dd7ce527e2785bcef810d8e3a4941
reshad21 <miakala7@gmail.com>
2026-04-30 11:38:02 +0600

    neown db database added

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
   res.json({
     success: true,
-    message: 'Welcome to the Backend in Nessa Foundation!',
+    message: 'Welcome to the Backend in Munnatv Islamic!',
   });
 });
 
